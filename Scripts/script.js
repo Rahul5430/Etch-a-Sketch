fc0a88d648bfd6ab1e9faa3f2453c33ce08b2c39
Rahul5430 <rahul213himani@gmail.com>
2020-08-14 12:56:14 +0530

    Change Colors is now working

--- Scripts/script.js
+++ Scripts/script.js
@@ -3,33 +3,16 @@ let cellColor = "#0310ea";
 let colorFill = "mono";
 let gridSize, borderWidth, totalWidth, cell, cells, cellLength;
 
-makeGrid();
-window.onresize = resizeCells();
 container.addEventListener("mouseover", changeColor);
-
-function  makeGrid() {
-    container.textContent = "";
-    gridSize = document.getElementById("gridSize").value || 16;
-    borderWidth = gridSize * 2;
-    totalWidth = document.getElementById("container").offsetWidth;
-    cellLength = (totalWidth - borderWidth) / gridSize;
-
-    for (i = 0; i < (gridSize * gridSize); i++) {
-        cell = document.createElement('div');
-        container.appendChild(cell);
-        cell.classList.add('cells');
-        cell.style.width = cellLength + "px";
-        cell.style.height = cellLength + "px";
-        cell.style.border = "1px solid #000000";
-    };
-};
+window.onresize = resizeCells();
+makeGrid();
 
 function resizeCells() {
     borderWidth = gridSize * 2;
     cells = document.getElementsByClassName("cells");
     totalWidth = document.getElementById("container").offsetWidth;
     cellLength = (totalWidth - borderWidth) / gridSize;
-    for (i = 0; i < cells.length; i++) {
+    for ( let i = 0; i < cells.length; i++) {
         cells[i].style.width = cellLength + "px";
         cells[i].style.height = cellLength + "px";
     };
@@ -37,7 +20,7 @@ function resizeCells() {
 
 function changeColor() {
     cells = document.getElementsByClassName("cells");
-    for (i = 0; i < cells.length; i++) {
+    for (let i = 0; i < cells.length; i++) {
         cells[i].addEventListener("mouseover", ()=>{
             switch (colorFill) {
                 case "mono":
@@ -48,6 +31,23 @@ function changeColor() {
                     break;
             }
         });
+    };
+};
+
+function  makeGrid() {
+    container.textContent = "";
+    gridSize = document.getElementById("gridSize").value || 16;
+    borderWidth = gridSize * 2;
+    totalWidth = document.getElementById("container").offsetWidth;
+    cellLength = (totalWidth - borderWidth) / gridSize;
+
+    for (let i = 0; i < (gridSize * gridSize); i++) {
+        cell = document.createElement('div');
+        container.appendChild(cell);
+        cell.classList.add('cells');
+        cell.style.width = cellLength + "px";
+        cell.style.height = cellLength + "px";
+        cell.style.border = "1px solid #000000";
     };
 };
 
